dfaa21a7b93cbb0a6d60a70bea74edebb342932d
mrm <ruiminm@kth.se>
2022-11-22 13:07:00 +0100

    webdesign

--- book_app/src/App.js
+++ book_app/src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import {Button} from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Button variant="primary"> Primary</Button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ruimin <code>jintng</code> Duosi
         </p>
         <a
           className="App-link"
@@ -15,7 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Peter
         </a>
       </header>
     </div>
