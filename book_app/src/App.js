39a9113809013710786e5cdf6614f102c39f4d7c
mrm <maruimin1998@gmail.com>
2022-11-22 17:37:06 +0100

    cashPNG

--- book_app/src/App.js
+++ book_app/src/App.js
@@ -1,18 +1,20 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import {Button} from 'react-bootstrap';
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import React, { Component } from 'react';
 import Header from "./components/Header";
 import HomeScreen from "./pages/HomeScreen";
+import {Container} from "react-bootstrap";
 
 class App extends Component {
   render() {
     return (
     <Router>
         <Header/>
+          <Container>
           <Routes>
               <Route exact path='/' element={<HomeScreen/>} />
           </Routes>
+          </Container>
       </Router>
     );
   }
