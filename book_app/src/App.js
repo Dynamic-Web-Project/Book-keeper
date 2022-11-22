95110ba58860a81405a17c883ca0045c83ef26de
mrm <maruimin1998@gmail.com>
2022-11-22 14:58:38 +0100

    change

--- book_app/src/App.js
+++ book_app/src/App.js
@@ -1,11 +1,20 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import {Button} from 'react-bootstrap';
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import HomeScreen from "./pages/HomeScreen";
-
+import Header from "./components/Header";
+import {Container} from "react-bootstrap";
 function App() {
   return (
-    <p>HHHH</p>
+    <Router>
+      <Header>
+        <Container>
+          <Routes>
+            <Route path='/' component={HomeScreen} exact/>
+          </Routes>
+        </Container>
+      </Header>
+    </Router>
   );
 }
 
