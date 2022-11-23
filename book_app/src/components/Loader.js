5bc386819d96e7450f55e185ece7595b014f1017
duosidai <daiduosi1999@gmail.com>
2022-11-23 16:32:15 +0100

    added

--- book_app/src/components/Loader.js
+++ book_app/src/components/Loader.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import {Spinner} from 'react-bootstrap';
 
@@ -15,20 +14,4 @@ export default function Loader({variant, className}) {
 Loader.defaultProps = {
     variant: 'dark',
     className: ''
-=======
-import React from 'react'
-import {Spinner} from 'react-bootstrap'
-
-export default function Loader({variant, className}){
-    return(
-        <Spinner className = {className} variant = {variant} animation = "border">
-            <span className = "visually-hidden"> Loading...</span>
-        </Spinner>
-    )
-}
-Loader.defaultProps = {
-    variant: 'dark',
-    className: '',
-    
->>>>>>> 93cc8b4435a32a353373f168865a39f834a4e1af
 }
--- book_app/src/pages/LoginScreen.js
+++ book_app/src/pages/LoginScreen.js
@@ -2,33 +2,19 @@ import React, { useState } from "react";
 import { Alert , Button, Form} from 'react-bootstrap';
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase";
-<<<<<<< HEAD
 import Loader from "../components/Loader"
 import {useNavigate} from "react-router-dom"
-=======
-import Loader from '../components/Loader';
-import { useNavigate } from "react-router-dom";
->>>>>>> 93cc8b4435a32a353373f168865a39f834a4e1af
 
 function LoginScreen() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState(null);
-<<<<<<< HEAD
     const [loading,setloading] = useState(false);
     const history = useNavigate();
 
     const handleSubmit = async (event) => {
         event.preventDefault();
         setloading(true);
-=======
-    const [loading, setLoading] = useState(false);
-    const history =useNavigate();
-
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        setLoading(true);
->>>>>>> 93cc8b4435a32a353373f168865a39f834a4e1af
 
         try{
             const res = await signInWithEmailAndPassword(auth, email, password);
@@ -36,25 +22,14 @@ function LoginScreen() {
         }catch(err){
           setError(err.message);
         }finally{
-<<<<<<< HEAD
             setloading(false);
-=======
-            setLoading(false);
->>>>>>> 93cc8b4435a32a353373f168865a39f834a4e1af
         }
     };
   return (
     <>
     <h1 className='fs-4'>Login</h1>
-<<<<<<< HEAD
     {loading && <Loader/>}
     {error && <Alert variant='danger'>{error}</Alert>}
-=======
-    {loading && <Loader />}
-    { error && (<Alert variant='danger'> {error} </Alert>
-        )
-    }
->>>>>>> 93cc8b4435a32a353373f168865a39f834a4e1af
     <Form onSubmit={handleSubmit}>
       <Form.Group className="mb-3">
         <Form.Label>Email</Form.Label>
