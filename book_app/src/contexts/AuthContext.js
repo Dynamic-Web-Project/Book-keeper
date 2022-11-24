b612e04d6783e24c9ce078650a26df0f1439d6c0
Jinting Zhang <zhangjinting118@gmail.com>
2022-11-24 09:20:36 +0100

    push check

--- book_app/src/contexts/AuthContext.js
+++ book_app/src/contexts/AuthContext.js
@@ -1,23 +1,53 @@
-import { signInWithEmailAndPassword } from 'firebase/auth';
-import React,{createContext,useContext} from 'react';
+import { setUserId } from 'firebase/analytics';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from 'firebase/auth';
+import React, { createContext, useContext, useEffect, useState , useReducer} from 'react';
 import { auth } from '../firebase';
 
 //test
 const initialState = {
-    currentUser:null
+    currentUser: null
 }
-
 const AuthContext = createContext()
-export function useAuth(){
+
+export function useAuth() {
     return React.useContext(AuthContext)
 }
 
-export function AuthProvider(props) {
-    const login = (email, password) => {
-         return signInWithEmailAndPassword(auth, email, password)
+function authReducer(state, state) {
+    switch (action.type) {
+        case 'LOGIN': return { ...state, currentUser: action.payload }
+        case 'LOGOUT': return { ...state, currentUser: null }
+        default: return state;
     }
-    const value={
-        currentUser: initialState.currentUser, login
+}
+
+export function AuthProvider(props) {
+    const [state, dispatch] = useReduceer(authReducer, initialState)
+    const [currentUser, setCurrentUser] = useState(initialState.currentUser);
+    const login = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    const signup = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    const logout = () => {
+        return signOut(auth)
+    }
+
+    useEffect(() => {
+        onAuthStateChanged(auth, user => {
+            if (user) {
+                setCurrentUser(user)
+            } else {
+                setCurrentUser(null)
+            }
+        })
+    }, [])
+
+    const value = {
+        currentUser, login, signup, logout
     }
     return (
         <AuthContext.Provider value={value} {...props} />
