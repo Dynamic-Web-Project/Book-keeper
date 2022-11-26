a43bf29c20c707e035c10e7e3c7cd3b26d1f02c9
mrm <maruimin1998@gmail.com>
2022-11-26 10:50:33 +0100

    title changed

--- book_app/src/views/HeaderView.js
+++ book_app/src/views/HeaderView.js
@@ -11,7 +11,7 @@ export default function HeaderView(props) {
           <a class="logo" href="/home">
             <Image src={logo} height="100" /></a>   
             <style>@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@1,900');</style>    
-            <span className="tittle">BOOK💯KEEPER </span>
+            <span className="tittle">BOOK🪙KEEPER </span>
 
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
