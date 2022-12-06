d2aede78fdddc58ff3e37f826e127df25a41a8a2
Peter Yuanzong Li <intsuki.yz@gmail.com>
2022-12-07 02:14:54 +0800

    fixed some errors that would appear in the console

--- book_app/src/views/HeaderView.js
+++ book_app/src/views/HeaderView.js
@@ -4,6 +4,27 @@ import { Container, Navbar, Nav, NavDropdown, Image } from "react-bootstrap";
 import logo from "../logo.png";
 
 export default function HeaderView(props) {
+
+    function renderUserMenu(props) {
+        if (props.user) {
+            return (
+                <NavDropdown title={props.user.email} id="basic-nav-dropdown" align="end">
+                    <NavDropdown.Item as={Link} to="/settings">Settings</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item as={Link} to="/login" onClick={props.logout}>Logout</NavDropdown.Item>
+                </NavDropdown>
+            )
+        } else {
+            return (
+                <>
+                    <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                    <Nav.Link as={Link} to="/signup">Sign Up</Nav.Link>
+                </>
+            )
+        }
+
+    }
+
     return (
         <div className="header-wrapper">
             <Navbar className="navbar">
@@ -16,26 +37,11 @@ export default function HeaderView(props) {
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto"></Nav>
                         <Nav>
-                            {props.user ? (
-                                <NavDropdown title={props.user.email} id="basic-nav-dropdown">
-                                    <NavDropdown.Item >
-                                        <Nav.Link as={Link} to="/settings" >Settings</Nav.Link>
-                                    </NavDropdown.Item>
-                                    <NavDropdown.Divider />
-                                    <NavDropdown.Item >
-                                        <Nav.Link as={Link} to="/login" onClick={props.logout}>Logout</Nav.Link>
-                                    </NavDropdown.Item>
-                                </NavDropdown>
-                            ) : (
-                                <>
-                                    <Nav.Link as={Link} to="/login">Login</Nav.Link>
-                                    <Nav.Link as={Link} to="/signup">Sign Up</Nav.Link>
-                                </>
-                            )}
+                            {renderUserMenu(props)}
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
-            </Navbar>
-        </div>
+            </Navbar >
+        </div >
     );
 }
