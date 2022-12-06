dd14d781d6ca9470796da63aabe64b7ac775f436
Peter Yuanzong Li <intsuki.yz@gmail.com>
2022-12-06 21:07:01 +0800

    testing the api and json responses

--- book_app/src/views/SettingsView.js
+++ book_app/src/views/SettingsView.js
@@ -5,13 +5,34 @@ export default function SettingsView(props) {
         if (error) return <div className="errorMessage"><span style={{ color: "white" }}>{props.error}</span></div>;
     }
 
+    // function renderResults(props) {
+    //     function renderTable(item) {
+    //         console.log(JSON.parse(item));
+    //     }
+
+    //     return (
+    //         // <span>
+    //         //     <p>query: {JSON.stringify(resultsArray.results[0].query)}</p>
+    //         //     <p>results/offers: {JSON.stringify(resultsArray.results[0].content.offers)}</p>
+    //         // </span>
+
+    //         <table>
+    //                 <thead></thead>
+    //                 <tbody>
+    //                     {/* {JSON.stringify(props.response[0]).map(renderTable)} */}
+    //                 </tbody>
+    //         </table>
+    //     )
+    // }
+
     return (
         <div className="settings-wrapper">
             <h1 className='fs-4'>Settings</h1>
             {handleErrorMessage(props.error)}
-            <button onClick={props.buttonClicked}>button</button>
             {props.loading && <Loading />}
             <p><label>Your UID is: {props.user.uid}.</label></p>
+            {/* <button onClick={props.buttonClicked}>button</button> */}
+            {/* <div><p>{renderResults(props)}</p></div> */}
         </div>
     );
 }
