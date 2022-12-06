5ee68cdd1769af000f3c6640b9d448213817537c
mrm <maruimin1998@gmail.com>
2022-12-06 22:00:11 +0100

    fic delete

--- book_app/src/views/HomeListView.js
+++ book_app/src/views/HomeListView.js
@@ -6,10 +6,7 @@ function HomeListView(props) {
     function handleErrorMessage(error) {
         if (error) return <div className="errorMessage"><span style={{ color: "white" }}>{props.error}</span></div>;
     }
-
-    function deleteBtnOnClicked(event){
-        return props.handelDelete;
-    }
+    
     function renderList(array) {
         function listRow(data) {
             return (
