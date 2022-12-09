b096cf0cf823ab7e02e2257f8c35b5e46843459a
Peter Yuanzong Li <intsuki.yz@gmail.com>
2022-12-09 09:42:32 +0800

    added paragraphing in about us

--- book_app/src/views/Aboutus.js
+++ book_app/src/views/Aboutus.js
@@ -12,14 +12,10 @@ export default () => {
                     <Card.Header className="top-header">Book-keeper</Card.Header>
                     <Card.Body>
                         <Card.Text className="top-text">
-                            What we decided to create was a live bookkeeping software website.
-                            The way users are able to store their bookkeeping records is by creating an account and logging in.
-                            This gives them the opportunity to edit their bookkeeping history,
-                            add and delete information including date, description,
-                            type and amount of income and expenses in real time.
-                            Users can also search for price comparisons for the same type of item in different stores and observe information about the item,
-                            such as the item name, the store that owns the item, and the price of the item in different stores.
-                            Users can use our price comparison analysis API to keep the information about the product they are interested in, such as the price and the name, in the interface, and then later, if the user buys the product, the product can be officially added to the P&L record.
+                            <p>What we decided to create was a live bookkeeping software website.</p>
+                            <p>The way users are able to store their bookkeeping records is by creating an account and logging in. This gives them the opportunity to edit their bookkeeping history, add and delete information including date, description, type and amount of income and expenses in real time.</p>
+                            <p>Users can also search for price comparisons for the same type of item in different stores and observe information about the item, such as the item name, the store that owns the item, and the price of the item in different stores.</p>
+                            <p>Users can use our price comparison analysis API to keep the information about the product they are interested in, such as the price and the name, in the interface, and then later, if the user buys the product, the product can be officially added to the P&L record.</p>
                         </Card.Text>
                     </Card.Body>
                 </Card>
